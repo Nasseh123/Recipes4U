6f64371b5815e13b118c31a47769e5a7b511d729
Adrian Etenyi <etadriano2@gmail.com>
2020-05-12 14:30:58 +0300

    add function to consume http request and return a mealplan class instance

--- app/requests.py
+++ app/requests.py
@@ -1,9 +1,15 @@
 import urllib.request,json
-
+from .models import  MealPlan
 # Getting api key
-api_key = None
-# Getting the movie base url
-base_url = None
+api_key = "6f8461162ff54814bf4501e258fa7a48"
 
-def configure_request(app):
-    pass
+url = "https://api.spoonacular.com/mealplanner/generate?apiKey={}"
+
+def get_mealplan():
+    """
+    Function to consume http request and return a Mealplan class instance
+    """
+    response = requests.get(url).json()
+
+    meal_result = MealPlan(response.get("title"),response.get("readyInMinutes"),response.get("servings"),response.get("sourceUrl"))
+    return meal_result
