e4fdd31c64cba73e0b8ad84e835b2147205bc9d6
Adrian Etenyi <etadriano2@gmail.com>
2020-05-12 14:51:05 +0300

    make minor adjustments

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from . import main
-
+from .models import MealPlan
+from ..requests import get_mealplan
 @main.route('/')
 def index():
     """
@@ -9,3 +10,8 @@ def index():
     title="RECIPE4U"
     
     return render_template('index.html',title=title)
+
+@main.route('/mealplan')
+def mealplan():
+    mealplan = get_mealplan()
+    return render_template('mealplan.html', title= "Mealplans", mealplan= mealplan)
